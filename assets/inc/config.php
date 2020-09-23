fb3716fa88a1e4456c0ede67ddab042891c35f62
George Popovic <67986283+gsmpopovic@users.noreply.github.com>
2020-09-23 19:03:59 -0400

    Update config.php
    Essentially hide contents of config.php

--- assets/inc/config.php
+++ assets/inc/config.php
@@ -3,12 +3,12 @@
 // Create constants for:
 // Consumer key, consumer secret key
 
-define('CONSUMER_KEY', '8JVa6gioaeihuAOl0YZ1fzU1g');
-define ('CONSUMER_SECRET', 'MsFMAiZdxL0HgLRaKKCOdQN5E7gpKT3TqInLlPk3vUvQY2R0oK');
+define('CONSUMER_KEY', '#');
+define ('CONSUMER_SECRET', '#');
 
 // Create variables for:
 // access token and access token secret
 
-$access_token='1306362620967096324-EskIU2jNCQWDBox8axiRKyiPoDonkt';
-$access_token_secret='HgUeFk4aw26P1YwdSiNZ4tZ3MQNshBjvGI4M9tmB4YuhX';
+$access_token='#';
+$access_token_secret='#';
 ?>
