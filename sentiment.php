ed5fbd468537961ef24f59821e7eac13e3deaff3
George Popovic <georgesmpopovic@gmail.com>
2020-09-24 18:11:42 -0400

    Edited processing func

--- sentiment.php
+++ sentiment.php
@@ -29,74 +29,75 @@ $json = new stdClass();
 // this data consist of twitter_datas containing all information 
 // relevant to a tweet 
 
-foreach($twitter_data as $k => $v){
+    foreach($twitter_data as $k => $v){
 
-    // For reference as to each tweet's contents, see the dataref.json 
+        // For reference as to each tweet's contents, see the dataref.json 
 
-    // Create an instance of a class which will contain 
-    // the highlights of each tweet, i.e., text, timestamp, author, location, 
-    // and its semantic analysis 
+        // Create an instance of a class which will contain 
+        // the highlights of each tweet, i.e., text, timestamp, author, location, 
+        // and its semantic analysis 
 
-    $tweet = new TweetAnalyzed();
+        $tweet = new TweetAnalyzed();
 
-    // Get tweet text
-    $tweet->tweet = $twitter_data[$k]->text; 
+        // Get tweet text
+        $tweet->tweet = $twitter_data[$k]->text; 
 
-    // Get tweet timestamp
-    $tweet->created_at=$twitter_data[$k]->created_at; 
+        // Get tweet timestamp
+        $tweet->created_at=$twitter_data[$k]->created_at; 
 
-    // Get tweet's author 
-    $tweet->author=$twitter_data[$k]->user->name; 
+        // Get tweet's author 
+        $tweet->author=$twitter_data[$k]->user->name; 
 
-    // Get author's twitter handle 
-    $tweet->handle=$twitter_data[$k]->user->screen_name; 
+        // Get author's twitter handle 
+        $tweet->handle=$twitter_data[$k]->user->screen_name; 
 
-    // Get location at which tweet created 
-    $tweet->location=$twitter_data[$k]->user->location; 
+        // Get location at which tweet created 
+        $tweet->location=$twitter_data[$k]->user->location; 
 
-    // Perform sentiment analysis
+        // Perform sentiment analysis
 
-    // This returns an associative array in the form of
-    // ['neg'=> 0.0, 'neu'=> 0.337, 'pos'=> 0.663, 'compound'=> 0.7096]
-    // Which are percentiles denoting how negative, neutral, positive 
-    // the sentiment of some piece of text is. 
-    // So: 0% negative; 33.7% neutral; 66.3% positive; 
-
-
-    // The Compound score is a metric that calculates the sum of all the lexicon ratings 
-    // which have been normalized 
-    // between -1(most extreme negative) and +1 (most extreme positive).
-
-    // positive sentiment : (compound score >= 0.05)
-    // neutral sentiment : (compound score > -0.05) and (compound score < 0.05)
-    // negative sentiment : (compound score <= -0.05)
+        // This returns an associative array in the form of
+        // ['neg'=> 0.0, 'neu'=> 0.337, 'pos'=> 0.663, 'compound'=> 0.7096]
+        // Which are percentiles denoting how negative, neutral, positive 
+        // the sentiment of some piece of text is. 
+        // So: 0% negative; 33.7% neutral; 66.3% positive; 
 
 
-    $analysis = $analyzer->getSentiment($tweet->tweet);
+        // The Compound score is a metric that calculates the sum of all the lexicon ratings 
+        // which have been normalized 
+        // between -1(most extreme negative) and +1 (most extreme positive).
 
-    // Percentage of negative sentiment 
-    $tweet->neg = $analysis['neg']*100;
-    // Percentage of neutral sentiment
-    $tweet->neu = $analysis['neu']*100; 
-    // Percentage of positive sentiment
-    $tweet->pos = $analysis['pos']*100; 
-    // Percentage of compound sentiment
-    $tweet->compound = $analysis['compound']*100; 
-    // Overall sentiment 
-    //
+        // positive sentiment : (compound score >= 0.05)
+        // neutral sentiment : (compound score > -0.05) and (compound score < 0.05)
+        // negative sentiment : (compound score <= -0.05)
 
-    // Cast index of our loop to string
-    // in order to index our JSON 
-    $key = strval($k); 
 
-    // Set each tweet within our JSON file 
-    $json->$key=$tweet; 
+        $analysis = $analyzer->getSentiment($tweet->tweet);
 
-}
+        // Percentage of negative sentiment 
+        $tweet->neg = $analysis['neg']*100;
+        // Percentage of neutral sentiment
+        $tweet->neu = $analysis['neu']*100; 
+        // Percentage of positive sentiment
+        $tweet->pos = $analysis['pos']*100; 
+        // Percentage of compound sentiment
+        $tweet->compound = $analysis['compound']*100; 
+        // Overall sentiment 
+        //
+
+        // Cast index of our loop to string
+        // in order to index our JSON 
+        $key = strval($k); 
+
+        // Set each tweet within our JSON file 
+        $json->$key=$tweet; 
+
+    }
 
     // Encode this and set into our JSON file 
 
     $json = json_encode($json); 
 
     file_put_contents('./assets/json/analysis.json', $json); 
+
 ?>
