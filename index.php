1262c67839b88acf5731d531deeacf01319e66e5
George Popovic <georgesmpopovic@gmail.com>
2020-09-26 11:27:07 -0400

    Added AJAX functionality.

--- index.php
+++ index.php
@@ -1,52 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+        <link rel="stylesheet" type="text/css" href="./assets/css/style.css" />
+        <link href="./assets/css/bootstrap.css" rel="stylesheet" type="text/css" />
+        <link href="./assets/css/bootstrap.css.map" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div class="container-fluid h-100">
+            <div class="row h-15">
+                <div class="col-md-12">
+                    <h1 style="text-align: center;">Tweetnalysis</h1>
+                </div>
+            </div>
+            <div class="row" id="index-body">
+                <div class="col-md-6">
+                    <form action="display.php" method="POST">
+                        <div class="form-group">
+                            <label for="searchbar-un">
+                                Enter a Twitter handle
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tweetnalysis | Home</title>
-    <link href="./assets/css/style.css" rel="stylesheet" type="text/css">
-    <link href="./assets/css/bootstrap.css" rel="stylesheet" type="text/css">
-    <link href="./assets/css/bootstrap.css.map" rel="stylesheet" type="text/css">
-</head>
+                                <input type="text" name="searchbar-un" class="form-control" id="searchbar-un" placeholder="@whomever" required />
+                            </label>
+                        </div>
+                        <div class="form-group">
+                            <label>
+                                How many tweets would you like to analyze?
 
-<body>
-    <div class="container-fluid d-md-flex h-md-100 align-items-center">
-    <div class="col-md-6">
-    <div class="col-md-12"><h1>Tweetnalysis</h1></div>    
-
-            <div>
-                <form action="display.php" method="POST">
-                    <div class="form-group">
-                        <label for='searchbar-un'>Enter a Twitter handle
-
-                            <input type='text' name='searchbar-un' class="form-control" id='searchbar-un' placeholder="@whomever" required>
-                        </label>
+                                <select name="searchbar-nm" class="form-control" id="searchbar-nm">
+                                    <option value="5">5</option>
+                                    <option value="10">10</option>
+                                    <option value="20">20</option>
+                                    <option value="30">30</option>
+                                    <option value="40">40</option>
+                                    <option value="50">50</option>
+                                    <option value="100">100</option>
+                                </select>
+                            </label>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="search" class="btn btn-primary" id="submit" />
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-6">
+                <div class="col-md-9 h-40">
+                        <div class="card">
+                            <div class="card-body">
+                                Tweetnalysis is a web application that retrieves tweets from a handle's timeline and analyzes their sentiment (the author's mood and attitude, as evidenced by their use of language). The PHP library is the work of David Oti, and can be found <a href="https://github.com/davmixcool/php-sentiment-analyzer">here</a>. The original project by C.J. Hutto et al. was written in Python, and can be found <a href="https://github.com/cjhutto/vaderSentiment">here</a>.
+                            </div>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label>How many tweets would you like to analyze?
-
-                            <select name="searchbar-nm" class="form-control" id='searchbar-nm'>
-                                <option value="5">5</option>
-                                <option value="10">10</option>
-                                <option value="20">20</option>
-                                <option value="30">30</option>
-                                <option value="40">40</option>
-                                <option value="50">50</option>
-                                <option value="100">100</option>
-                            </select>
-                        </label>
+                    <div class="col-md-9 h-40">
+                        <div class="card">
+                            <div class="card-body">
+                                Sentiment analysis is performed using a VADER (Valence Aware Dictionary and SEntiment Reasoner) approach, where each tweet is decomposed and each word checked against a lexicon that which lists the sentiment of common words--whether, positive (e.g., "good"), negative (e.g., "bad"), or neutral (i.e., no strong attitude indicated). Words are checked both for polarity (whether good or bad) and their intensity (how good or bad). The overall sentiment of each tweet is broken down into percentages based on these scores.
+                            </div>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <input type="submit" name="search" class="btn btn-primary" id="submit">
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
-        <div class="col-md-6">
-        </div>
-    </div>
-    <script src="assets/js/script.js"></script>
-</body>
-
+        <script src="assets/js/script.js"></script>
+    </body>
 </html>
