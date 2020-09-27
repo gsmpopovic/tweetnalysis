53b6d4b7ee5376fa8ea1cc8ac09bdabddbc96e02
George Popovic <georgesmpopovic@gmail.com>
2020-09-26 21:20:38 -0400

    Added AJAX functionality.

--- index.php
+++ index.php
@@ -10,23 +10,23 @@
     </head>
     <body>
         <div class="container-fluid h-100">
-            <div class="row h-15">
+            <div class="row h-15 mb-md-2">
                 <div class="col-md-12">
-                    <h1 style="text-align: center;">Tweetnalysis</h1>
+                    <h1 class="h1 display-2" style="text-align: center;">Tweetnalysis</h1>
                 </div>
             </div>
             <div class="row" id="index-body">
                 <div class="col-md-6">
-                    <form action="display.php" method="POST">
+                    <form action="display.php" method="POST" class="p-5 bg-primary rounded text-light">
                         <div class="form-group">
-                            <label for="searchbar-un">
-                                Enter a Twitter handle
+                            <label for="searchbar-un" class="display-6"> 
+                                Enter a Twitter handle:
 
                                 <input type="text" name="searchbar-un" class="form-control" id="searchbar-un" placeholder="@whomever" required />
                             </label>
                         </div>
                         <div class="form-group">
-                            <label>
+                            <label class="display-6">
                                 How many tweets would you like to analyze?
 
                                 <select name="searchbar-nm" class="form-control" id="searchbar-nm">
@@ -41,19 +41,19 @@
                             </label>
                         </div>
                         <div class="form-group">
-                            <input type="submit" name="search" class="btn btn-primary" id="submit" />
+                            <input type="submit" name="search" class="btn btn-secondary" id="submit" />
                         </div>
                     </form>
                 </div>
                 <div class="col-md-6">
-                <div class="col-md-9 h-40">
+                <div class="col-md-9 h-40 my-md-2">
                         <div class="card">
                             <div class="card-body">
                                 Tweetnalysis is a web application that retrieves tweets from a handle's timeline and analyzes their sentiment (the author's mood and attitude, as evidenced by their use of language). The PHP library is the work of David Oti, and can be found <a href="https://github.com/davmixcool/php-sentiment-analyzer">here</a>. The original project by C.J. Hutto et al. was written in Python, and can be found <a href="https://github.com/cjhutto/vaderSentiment">here</a>.
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-9 h-40">
+                    <div class="col-md-9 h-40 my-md-2">
                         <div class="card">
                             <div class="card-body">
                                 Sentiment analysis is performed using a VADER (Valence Aware Dictionary and SEntiment Reasoner) approach, where each tweet is decomposed and each word checked against a lexicon that which lists the sentiment of common words--whether, positive (e.g., "good"), negative (e.g., "bad"), or neutral (i.e., no strong attitude indicated). Words are checked both for polarity (whether good or bad) and their intensity (how good or bad). The overall sentiment of each tweet is broken down into percentages based on these scores.
