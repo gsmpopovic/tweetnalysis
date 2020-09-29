21130fcf315bb465aca085c6bc8e63ba3a3f4bec
George Popovic <georgesmpopovic@gmail.com>
2020-09-29 12:23:01 -0400

    Attempting to add AJAX error handling.

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
 // Create variables for:
 // Consumer key, consumer secret key
 
-$cons_key='';
-$cons_secret='';
+$cons_key='vom7lRtJgj7pQvPVcCtLmCqs7';
+$cons_secret='fB4qsNON330cXfurOmmYtEtD9jAOi06p0YzhQMlMNyPyBJBOYK';
 // Create variables for:
 // access token and access token secret
 
-$access_token='';
-$access_token_secret='';
+$access_token='1306362620967096324-7xKWF8XDUKPUrw8DbPiWStpYO5DiDD';
+$access_token_secret='Q1afUHfuXbL6ES6KAzOG30RaP6K0hOCP5qODC01a7p5W9';
 ?>
