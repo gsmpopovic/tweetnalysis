39f5335376adea148ded9ad377ee83e05c0ccfef
George Popovic <georgesmpopovic@gmail.com>
2020-10-04 22:58:55 -0400

    Postgresql processing functionality

--- assets/inc/inpsql.php
+++ assets/inc/inpsql.php
@@ -1,32 +1,23 @@
 <?php 
 
-// This functionality is secondary; 
-// All data which results from the user's query will be stored in a JSON file 
-// And persist as long as their session 
+// Store in a PostGRES database hosted via Heroku
 
-// Store in a MySQL database
+$dsn = "pgsql:host=ec2-34-235-62-201.compute-1.amazonaws.com;dbname=d4cde6k5s3uff8";
+$username = "develtqorjkwrw";
+$password = "0b4a7dd7df8f80831d5502f72ef7bdac2f252c2a2cc0ef44fce21bab03a84947";
+$tablename = "tweets"; 
 
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'twitter';
-$tablename = 'tweets';
+try {
+    $connection = new PDO($dsn, $username, $password);
+    $dconnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$connection = new mysqli($host, $username, $password, $database);
-
-if (!$connection){
-    die($connection->connect_error);
-}
-
-else{
-
+    
     // The API request returns an array of objects, each of which represents a single tweet
     // Therefore, to grab each individual tweet and inject it into a database, 
     // You have to iterate through the array, and access
     // the properties of each object 
 
     foreach($tweets as $k => $v) {
-
         // The author's name is located in the name property of the user object,
         // The author's location is, ibid.,
         // Both are located inside the tweet object,
@@ -59,5 +50,9 @@ else{
         
         }
     }
+
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+    exit; 
 }
 ?>
